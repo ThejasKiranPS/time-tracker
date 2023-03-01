4b6e9cdc150e7506d39098e5e3271ee9e9740007
Thejas Kiran P S <thejaskiranps@gmail.com>
2023-03-01 23:14:47 +0530

    display time duration from start to end

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
-use chrono::{format::Item, Local};
+use chrono::{Duration, Local, NaiveDateTime};
+use rev_lines::RevLines;
+use std::io::BufReader;
 use std::{
     fs::{File, OpenOptions},
     io::Write,
     path::Path,
 };
+const TIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
 pub struct Config {
     pub path: String,
 }
@@ -31,12 +34,14 @@ fn open_file(path: &str) -> File {
     let path = Path::new(&path);
     if path.exists() {
         OpenOptions::new()
+            .read(true)
             .append(true)
             .open(&path)
             .expect("Could not open file!")
     } else {
         OpenOptions::new()
             .create(true)
+            .read(true)
             .append(true)
             .open(&path)
             .expect("Could not open file!")
@@ -44,7 +49,7 @@ fn open_file(path: &str) -> File {
 }
 
 /// Logs current time along with the `mode` to `file`
-pub fn write(mut file: File, mode: String) {
+pub fn write(mut file: &File, mode: &str) {
     file.write_fmt(format_args!("{0} {1}\n", get_current_time(), mode))
         .expect("Could not write to file");
 }
@@ -57,14 +62,56 @@ pub fn log(args: Args) -> Result<(), &'static str> {
     let mode = args.mode;
     let file = open_file("hello.txt");
 
-    write(file, mode);
+    write(&file, &mode);
+    if mode == "end" {
+        let duration = calculate_time_difference(&file).unwrap();
+        println!("{}", format_duration(duration));
+    }
     Ok(())
 }
 
-pub fn calculate_time_difference() {}
+pub fn calculate_time_difference(file: &File) -> Option<chrono::Duration> {
+    let mut rev_lines = RevLines::new(BufReader::new(file)).unwrap();
+    let end_line = rev_lines.next()?;
+
+    let (date_time, mode) = parse_line(end_line).unwrap();
+    let end_time = parse_time(&date_time);
+    if mode != "end" {
+        panic!("End record wasn't logged properly");
+    }
+
+    for line in rev_lines {
+        let (date_time, mode) = parse_line(line).unwrap();
+        if mode == "start" {
+            let start_time = parse_time(&date_time);
+            return Some(end_time - start_time);
+        }
+    }
+    None
+}
+
+fn parse_time(date_time: &str) -> NaiveDateTime {
+    NaiveDateTime::parse_from_str(&date_time, TIME_FORMAT).expect("Could not parse time")
+}
+
+fn parse_line(line: String) -> Option<(String, String)> {
+    let mut parsed_line = line.split(' ');
+    let date = parsed_line.next()?;
+    let time = parsed_line.next()?;
+    let mode = parsed_line.next()?;
+
+    Some((format!("{} {}", date, time), mode.to_string()))
+}
 
 /// Returns current date & time in a readable format
 pub fn get_current_time() -> String {
     let now = Local::now();
-    format!("{}", now.format("%Y-%m-%d %H:%M:%S"))
+    format!("{}", now.format(TIME_FORMAT))
+}
+
+fn format_duration(duration: Duration) -> String {
+    let seconds = duration.num_seconds() % 60;
+    let minutes = duration.num_minutes();
+    let hours = duration.num_hours();
+    format!("{}h {}m {}s", hours, minutes, seconds)
 }
